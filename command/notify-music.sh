6a58d38eeb06b58fbe2dd1160ea220303024b66c
ekickx <mujahidmasadepan@gmail.com>
2018-05-18 20:17:44 +0700

    change time when image close from 3 to 5

--- command/notify-music.sh
+++ command/notify-music.sh
@@ -26,7 +26,7 @@ then
 	notify-send "$(echo -e "$(quodlibet --print-playing "<title>")\n$(quodlibet --print-playing "<artist|<artist>|Unknown Artist>")")" "$(quodlibet --print-playing "<~rating>")" 
 	if [[ -e ~/.quodlibet/art/"$(echo "`(quodlibet --print-playing "<title>")`.png")" ]]; then
 		n30f -x 27 -y 573 ~/.quodlibet/art/"$(echo "`(quodlibet --print-playing "<title>")`.png")"&
-		sleep 3
+		sleep 5
 		pkill n30f
 		exit;
 	else
@@ -34,12 +34,12 @@ then
 			convert -resize 140x140 -border 5x5 -bordercolor "#E8EAF9" ~/.quodlibet/current.cover "$(echo "`(quodlibet --print-playing "<title>")`.png")"
 			mv "$(echo "`(quodlibet --print-playing "<title>")`.png")" ~/.quodlibet/art/
 			n30f -x 27 -y 573 ~/.quodlibet/art/"$(echo "`(quodlibet --print-playing "<title>")`.png")"&
-			sleep 3
+			sleep 5
 			pkill n30f	
 			exit;		
 		else
 			n30f -x 27 -y 573 ~/.quodlibet/art/unknown.png&
-			sleep 3
+			sleep 5
 			pkill n30f
 			exit;
 		fi
